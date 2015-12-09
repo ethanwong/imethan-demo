c6dd22482f5b17586f584b45d286d45a21e8d5eb
EthanWong <ethanwong@qq.com>
2015-12-09 10:50:29 +0800

    项目目录整理

---
+++ imethan-webflow/src/test/java/cn/imethan/webflow/test/TestController.java
@@ -0,0 +1,22 @@
+package cn.imethan.webflow.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * TestController.java
+ *
+ * @author Ethan Wong
+ * @since JDK 1.7
+ * @datetime 2015年12月5日下午5:35:51
+ */
+@Controller
+@RequestMapping("/test")
+public class TestController {
+	
+	@RequestMapping(value="")
+	public String test(){
+		return "test";
+	}
+
+}
