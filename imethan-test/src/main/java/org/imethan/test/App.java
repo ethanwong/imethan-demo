d3e13d7140f1764dab675aa368547c8fac2fc909
EthanWong <ethanwong@qq.com>
2016-03-16 15:13:43 +0800

    添加imethan-mina项目

--- imethan-test/src/main/java/org/imethan/test/App.java
+++ imethan-test/src/main/java/org/imethan/test/App.java
@@ -1,13 +1,27 @@
 package org.imethan.test;
 
+import junit.framework.Assert;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+	public static void main(String args[]) {
+		int i = 1;
+        //断言1结果为true，则继续往下执行
+        assert i > 2:"i不大于1";
+        
+        Assert.assertEquals("i不大于1",i > 2, true);
+        
+        
+        System.out.println("断言1没有问题，Go！");
+ 
+        System.out.println("\n-----------------\n");
+ 
+        //断言2结果为false,程序终止
+        assert false : "断言失败，此表达式的信息将会在抛出异常的时候输出！";
+        System.out.println("断言2没有问题，Go！");
     }
 }
